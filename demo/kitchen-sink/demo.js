b3e454a944de94cb437be66c9bd763d314368abb
Jim Buck <jim@jimmyboh.com>
2014-04-17 11:56:50 -0400

    Fixed hotkeys for changing font size (Fixes #1914)

--- demo/kitchen-sink/demo.js
+++ demo/kitchen-sink/demo.js
@@ -184,10 +184,11 @@ env.editor.commands.addCommands([{
     }
 }, {
     name: "increaseFontSize",
-    bindKey: "Ctrl-+",
+    bindKey: "Ctrl-=",
     exec: function(editor) {
         var size = parseInt(editor.getFontSize(), 10) || 12;
         editor.setFontSize(size + 1);
+        return;
     }
 }, {
     name: "decreaseFontSize",
@@ -195,12 +196,14 @@ env.editor.commands.addCommands([{
     exec: function(editor) {
         var size = parseInt(editor.getFontSize(), 10) || 12;
         editor.setFontSize(Math.max(size - 1 || 1));
+        return;
     }
 }, {
     name: "resetFontSize",
     bindKey: "Ctrl+0",
     exec: function(editor) {
         editor.setFontSize(12);
+        return;
     }
 }]);
 
